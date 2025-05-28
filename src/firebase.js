878fa144e0ed40a4f09c1fd572aa757369b3c846
Keidy Juliana <KJ.GONZALEZSANCHEZUNICIENCIABGA.EDU.CO>
2025-05-28 15:00:46 -0500

    corrige errores para vercel

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-$ npm install firebase
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -14,6 +9,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID
 };
+
 const app = initializeApp(firebaseconfig);
-const db = getFirestore(app)
-expor { db };
+export const db = getFirestore(app);
