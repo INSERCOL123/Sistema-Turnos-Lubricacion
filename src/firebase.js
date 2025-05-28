6120c336705db1822d427bd2103398085027d141
INSERCOL123 <CONTABILIDAD2@INSERCOL.COM.CO>
2025-05-28 15:42:33 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD_M0TkSeJc_MUleIBK1b2Y9dq1puf-8Ww",
   authDomain: "turnos-lubricacion-6998a.firebaseapp.com",
   projectId: "turnos-lubricacion-6998a",
-  storageBucket: "turnos-lubricacion-6998a.firebasestorage.app",
+  storageBucket: "turnos-lubricacion-6998a.appspot.com",
   messagingSenderId: "21521100171",
   appId: "1:21521100171:web:b84b49500bc9c3ed81138e"
 };
